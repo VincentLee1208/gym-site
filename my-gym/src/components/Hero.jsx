aead6b3f2081d47fa72245206e06d25daa6ef58f
VincentLee1208 <vinlee1208@gmail.com>
2023-05-07 21:43:11 -0700

    Hero Banner Finished

--- my-gym/src/components/Hero.jsx
+++ my-gym/src/components/Hero.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 
 import '../styles/Hero.css'
-/*import BackgroundImg from '../assets/hero/hero7.jpg'*/
+
 
 const Hero = () => {
   return <div className='hero_section'>
-    
+    <div className='hero_container'>
+        <div className='hero_content_left'>
+            <center className='title'>
+                APEX
+            </center>
+            <center className='subtitle'>
+                SPORTS SYSTEMS
+            </center>
+        </div>
+
+        <div className='hero_content_right'>
+            <p className='quote'>
+                YOUR ONLY LIMIT IS THE ONE YOU SET FOR YOURSELF - PUSH BEYOND IT AND UNLOCK YOUR TRUE POTENTIAL
+            </p>
+
+            <ul className='hero_list'>
+                <li>
+                    NUTRITION
+                </li>
+
+                <li>
+                FITNESS
+                </li>
+
+                <li>
+                RECOVERY
+                </li>
+            </ul>
+        </div>
+    </div>
   </div>
 }
 
