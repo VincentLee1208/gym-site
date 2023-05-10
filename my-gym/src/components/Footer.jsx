16b8061870934458bd49283dcfc1c8ff6a203bfb
VincentLee1208 <vinlee1208@gmail.com>
2023-05-10 10:24:22 -0700

    footer start

--- my-gym/src/components/Footer.jsx
+++ my-gym/src/components/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
+import Logo from '../assets/logo/logo.png'
+
+
+import '../styles/Footer.css'
+
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+  return <div className='footer_section'>
+    <div className='footer_logo'>
+      <img className='logo' src={Logo} alt='logo_img'/>
+    </div>
+
+    <div className='footer_socialmedia'>
+
+
+    </div>
+
+    <div className='footer_information'>
+      <p>
+
+      </p>
+    </div>
+  </div>
 }
 
 export default Footer
